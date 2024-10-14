876883e82bc2c11e968ce83182242a40ad57d198
lai2-24 <lai2-24@itismeucci.com>
2024-10-14 09:36:27 +0200

    esercizio modificato

--- src/main/java/lopreiato/leon/Main.java
+++ src/main/java/lopreiato/leon/Main.java
@@ -6,26 +6,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
-        System.out.println("Il client è partito");
-
-        Socket mySocket = new Socket("localhost", 5637);
-
+        Socket s = new Socket("localhost", 5637);
         System.out.println("Il client si è collegato");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
-        DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
 
-        out.writeBytes("ti sei collegato '\n'");
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-        String stringaCambiata = in.readLine();
+        String stringaInviata;
+        String stringaRicevuta;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Inserire una stringa per terminare l'esecuzione");
+            stringaInviata = scanner.nextLine();
+            out.writeBytes(stringaInviata +"\n");
+            stringaRicevuta = in.readLine();
+        } while (stringaInviata.equals("0"));
 
-        System.out.println("Nuova stringa " + stringaCambiata);
 
-        mySocket.close();
 
+        out.writeBytes("sono il Client" + "\n");
+
+        String stringRicevuta = in.readLine();
+        System.out.println("La stringa ricevuta: " + stringRicevuta);
+
+        s.close();
+
+
+
+        
 
 
     }
